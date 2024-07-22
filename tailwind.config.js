0b163c226aa520dea40684bfd7fd7ca162400631
IbiminaFG <gogoibimina@gmail.com>
2024-07-22 16:20:30 +0100

    Create the vite structure for the website

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primaryExtraLight: ["LufgaExtraLight"],
+        primaryLight: ["LufgaLight"],
+        primaryMedium: ["LufgaMedium"],
+        primaryRegular: ["LufgaRegular"],
+        primarySemiBold: ["LufgaSemiBold"],
+        primaryBold: ["lufgaBold"],
+        primaryExtraBold: ["LufgaExtraBold"],
+      },
+    },
+  },
+  plugins: [],
+};
