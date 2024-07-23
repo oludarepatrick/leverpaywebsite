d5bb39003a65425b18d7970b5cd39d5fc57296a5
IbiminaFG <gogoibimina@gmail.com>
2024-07-23 23:33:13 +0100

    Create the hero section including the navigations

---
+++ src/components/Features.jsx
@@ -0,0 +1,13 @@
+const Features = () => {
+  return (
+    <div>
+      <h2>Feautures</h2>
+      <p>
+        We are a company dedicated to providing the best services to our
+        customers.
+      </p>
+    </div>
+  );
+};
+
+export default Features;
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,13 @@
+const Pricing = () => {
+  return (
+    <div>
+      <h2>Pricing</h2>
+      <p>
+        We are a company dedicated to providing the best services to our
+        customers.
+      </p>
+    </div>
+  );
+};
+
+export default Pricing;
