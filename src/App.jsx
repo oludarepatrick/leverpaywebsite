ba0f9bac3b244943bd9e43989ecd16c68f48c4f5
IbiminaFG <gogoibimina@gmail.com>
2024-07-23 23:53:38 +0100

    Fix merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
-import Pricing from "./components/Pricing";
-import Features from "./components/Features";
+// import Navbar from "./components/Navbar";
+// import HeroSection from "./components/HeroSection";
+// import Pricing from "./components/Pricing";
+// import Features from "./components/Features";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <div className="content-grid all | min-h-dvh">
       <div className="screen_height w-full p-4 sm:py-11 flex flex-col gap-8 overflow-hidden relative">
         <div className="absolute -z-[1] -top-44 -left-28 bg-brown-shade-0 blur-[150px] w-[444px] h-[444px] rounded-full"></div>
         <div className="absolute -z-[1] -bottom-44 -right-28 bg-blue-shade-0 blur-[150px] w-[444px] h-[444px] rounded-full"></div>
@@ -28,14 +28,15 @@ function App() {
           <p>This is a simple landing page with a responsive header.</p>
         </div>
       </main>
-    </>
-    <div className="content-grid all | min-h-dvh">
-      <Navbar/>
-      <HeroSection/>
-      <Pricing/>
-      <Features/>
-      <Footer/>
+      <Footer />
     </div>
+    // <div className="content-grid all | min-h-dvh">
+    //   <Navbar/>
+    //   <HeroSection/>
+    //   <Pricing/>
+    //   <Features/>
+    //   <Footer/>
+    // </div>
   );
 }
 
