d043fca21b7a41be2f9fcf097e34b4bf6fb99874
oludarepatrick <78766589+oludarepatrick@users.noreply.github.com>
2024-08-05 12:18:28 +0100

    updated account deactivation link

--- src/pages/Deactivation.jsx
+++ src/pages/Deactivation.jsx
@@ -1,6 +1,7 @@
 const Deactivation = () => {
     return (
-        <div className="container mx-auto h-screen p-4 bg-white text-blue-shade-20">
+        <div className="bg-blue-500 py-12 min-h-screen">
+            <div className="container mx-auto p-6 bg-white shadow-lg rounded-lg">
            <h1 className="font-primarySemiBold md:text-3xl text-2xl py-4">Account Deactivation!</h1>
            <h3 className="font-primarySemiBold text-xl py-4">
                 We will like to know why you want to close your account with us
@@ -10,9 +11,9 @@ const Deactivation = () => {
            </p>
            <p className="text-justify pb-2 leading-7 font-primaryRegular">
                 You can also send us a Whatsapp message or request for a callback at: +2349060898687
-           </p>           
+           </p>  
+            </div>        
         </div>
     )
 }
 export default Deactivation;
-
